701914ce4c8650a95758b79f0ceda47454063687
Jeff Marks <JKDMarks@gmail.com>
2024-04-09 16:33:14 -0400

    Add vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { readFileSync } from "fs";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  const config: UserConfig = {
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
+  };
+
+  if (process.env.VITE_USE_HTTPS === "true") {
+    config.server.https = {
+      key: readFileSync(process.env.HOME + "/certs/key.pem"),
+      cert: readFileSync(process.env.HOME + "/certs/cert.pem"),
+    };
+  }
+
+  return config;
 });
