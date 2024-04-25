580e2ffb135f5b2034a9a3d64aea0572279479ee
Jeff Marks <JKDMarks@gmail.com>
2024-04-25 11:39:44 -0400

    Scroll timeout 200ms

--- src/pages/Games/NewGameSinglePlayerDeck.tsx
+++ src/pages/Games/NewGameSinglePlayerDeck.tsx
@@ -82,13 +82,13 @@ export default function NewGameSinglePlayerDeck({
   };
 
   const scrollIntoView = (event: React.FocusEvent<HTMLDivElement>) => {
-    event.stopPropagation();
-    event.target.focus({ preventScroll: true });
+    // event.stopPropagation();
+    // event.target.focus({ preventScroll: true });
     const domRect = event.target.getBoundingClientRect();
     const eltDistToTopOfScreen = domRect.top;
-    // setTimeout(() => {
-    window.scrollTo({ top: window.scrollY + eltDistToTopOfScreen });
-    // }, 50);
+    setTimeout(() => {
+      window.scrollTo({ top: window.scrollY + eltDistToTopOfScreen });
+    }, 200);
   };
 
   return (
